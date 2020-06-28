c62c99ef22ba7319671d92cb704526eebacc2cf4
deepu <deepu.codex@gmail.com>
2020-06-28 16:09:24 +0530

    commit

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,23 +1,14 @@
-
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 const App = () => {
-    return React.createElement(
-        "div", { id: "my-root-div" },
-        [
-            React.createElement("h1", {}, "Hello React!!"),
-            React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Dabber" }),
-            React.createElement(Pet, { name: "Caty", animal: "Cat", breed: "Mamal" }),
-            React.createElement(Pet, { name: "Pipper", animal: "Bird", breed: "Mixed" })
-        ]
-    );
+  return (
+    <div id="my-root-div">
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Dabber" />
+      <Pet name="Caty" animal="Cat" breed="Mamal" />
+      <Pet name="Pipper" animal="Bird" breed="Mixed"></Pet>
+    </div>
+  );
 };
-const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {},
-        [
-            React.createElement("h1", {}, name),
-            React.createElement("h2", {}, animal),
-            React.createElement("h2", {}, breed)
-        ]
-
-    )
-}
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
